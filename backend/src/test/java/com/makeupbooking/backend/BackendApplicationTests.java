0eeb70361161110d2dfb578c75722ac1e42efa86
Shashi Chaubey <kumarshashi8104@gmail.com>
2026-03-16 00:02:51 +0530

    feat: implement authentication with BCrypt and JWT and 2 apis

---
+++ backend/src/test/java/com/makeupbooking/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makeupbooking.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
